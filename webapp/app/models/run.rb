c3be39199090c54aa6264e9f223c200c29bbd508
Andrew Sulistio <andrew.sulistio@me.com>
2019-01-12 13:19:18 -0800

    Update run.rb

--- webapp/app/models/run.rb
+++ webapp/app/models/run.rb
@@ -1,9 +1,9 @@
 class Run < Sequel::Model
   RUN_STATUS = {
-    0 = "Pending"
-    1 = "Ordered"
-    2 = "In transit"
-    3 = "Out for Delivery"
-    4 = "Avaliable for Pickup"
+    0 => "Pending"
+    1 => "Ordered"
+    2 => "In transit"
+    3 => "Out for Delivery"
+    4 => "Avaliable for Pickup"
     }
 end
